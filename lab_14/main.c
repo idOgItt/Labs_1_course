a31ffcaafabb4cfe0eeb09612a18e712cb111dda
idOgItt <sergeylkd@bk.ru>
2023-10-30 00:11:55 +0300

    Address arithmetics

--- lab_14/main.c
+++ lab_14/main.c
@@ -1,116 +1,81 @@
-// 26
 #include <stdio.h>
 
-typedef enum _kDir
-{
-	LEFT = 0,
-	UP,
-	RIGHT,
-	DOWN
+typedef enum _kDir {
+    LEFT = 0,
+    UP,
+    RIGHT,
+    DOWN
 } kDir;
 
-int main(void)
-{
-	const int N = 7;
-	int n, a[N][N], i, j, k, leftInd, rightInd, upInd, downInd;
-	kDir dir;
+int main(void) {
+    const int N = 7;
+    int n, a[N][N], k;
+    kDir dir;
 
-	while (1)
-	{
-		printf("Введите n (1 - %d. Завершить - 0): ", N);
-		scanf("%d", &n);
-		
-		if (n == 0) break;
+    while (1) {
+        printf("Введите n (1 - %d, Завершить - 0): ", N);
+        scanf("%d", &n);
 
-		if (n < 1 || n > N)
-		{
-			printf("Недопустимое значение n\n");
+        if (n == 0)
+            break;
 
-			continue;
-		}
+        if (n < 1 || n > N) {
+            printf("Недопустимое значение n\n");
+            continue;
+        }
 
-		for (i = 0; i < n; i++)
-			for (j = 0; j < n; j++)
-				scanf("%d", &a[i][j]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                scanf("%d", (*(a + i) + j));
+            }
+        }
 
-		printf("Исходная матрица:\n");
+        printf("Исходная матрица:\n");
 
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++) printf("%d ", a[i][j]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%d ", *(*(a + i) + j));
+            }
+            printf("\n");
+        }
 
-			printf("\n");
-		}
+        dir = RIGHT;
+        int *ptr = *a;
 
-		dir = LEFT;
-		leftInd = 0;
-		rightInd = n - 1;
-		upInd = 0;
-		downInd = n - 1;
-		i = n - 1;
-		j = n - 1;
+        printf("Линейный вид: ");
 
-		printf("Линейный вид: ");
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
 
-		for (k = 0; k < n * n; k++)
-		{
-			printf("%d ", a[i][j]);
+        while (left <= right && top <= bottom) {
+            if (dir == LEFT) {
+                for (int i = right; i >= left; i--) {
+                    printf("%d ", *(*(a + bottom) + i));
+                }
+                bottom--;
+                dir = UP;
+            } else if (dir == UP) {
+                for (int i = bottom; i >= top; i--) {
+                    printf("%d ", *(*(a + i) + left));
+                }
+                left++;
+                dir = RIGHT;
+            } else if (dir == RIGHT) {
+                for (int i = left; i <= right; i++) {
+                    printf("%d ", *(*(a + top) + i));
+                }
+                top++;
+                dir = DOWN;
+            } else if (dir == DOWN) {
+                for (int i = top; i <= bottom; i++) {
+                    printf("%d ", *(*(a + i) + right));
+                }
+                right--;
+                dir = LEFT;
+            }
+        }
 
-			switch (dir)
-			{
-				case LEFT:
-				{
-					if (j > leftInd) j--;
-					else
-					{
-						i--;
-						downInd--;
-						dir = UP;
-					}
-				}
-				break;
+        printf("\n");
+    }
 
-				case UP:
-				{
-					if (i > upInd) i--;
-					else
-					{
-						j++;
-						leftInd++;
-						dir = RIGHT;
-					}
-				}
-				break;
-
-				case RIGHT:
-				{
-					if (j < rightInd) j++;
-					else
-					{
-						i++;
-						upInd++;
-						dir = DOWN;
-					}
-				}
-				break;
-
-				case DOWN:
-				{
-					if (i < downInd) i++;
-					else
-					{
-						j--;
-						rightInd--;
-						dir = LEFT;
-					}
-				}
-				break;
-			}
-		}
-
-		printf("\n");
-	}
-
-	return 0;
+    return 0;
 }
-
