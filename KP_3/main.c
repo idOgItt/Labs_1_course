bbac872a409651ce0d2dd122d3a530681e649e89
idOgItt <sergeylkd@bk.ru>
2023-10-30 01:12:44 +0300

    Reworked

--- KP_3/main.c
+++ KP_3/main.c
@@ -1,61 +1,59 @@
-/*var 24*/
 #include <stdio.h>
 #include <math.h>
 
-int fact(int n)
+long double machine_epsilon(long double x)
 {
-    if (n == 0 || n==1)
-        return 1;
-    else
-    {
-        return n * fact(n - 1);
-    }
-}
-
-long double machine_epsilon(void)
-{
-	long double e = 1.0f;
-	while (1.0f + e / 2.0f > 1.0f)
-		e /= 2.0f;
-	return e;
+    long double e = 1.0L;
+    while (x + e / 2.0L > x)
+        e /= 2.0L;
+    return e;
 }
 
 int main()
 {
-	printf("Введите количество равных разбиений отрезка [a, b]: n = ");
+    printf("Введите значение x: ");
+    long double x;
+    scanf("%Lf", &x);
+
 	int n;
-	scanf("%d", &n);
-	if (n < 1) {
-		printf("Введенное значение должно быть больше нуля!\n");
-		return 0;
-	}
-	double e0 = machine_epsilon();
-	double d = 1 / (double)n;
-	printf("Машинное эпсилон для типа long double: e = %.8e\n", e0);
-	printf("+-----------------------------------------------------------------------------------------------------+\n");
-	printf("|            Таблица значений ряда Тейлора и стандартной функции для f(x) = exp(pow(x, 2))            |\n");
-	printf("+-------+-----------------------------------------+---------------------------------+-----------------+\n");
-	printf("|   x   | частичная сумма ряда для exp(pow(x, 2)) |      значение функции f(x)      | число итераций  |\n");
-	printf("+-------+-----------------------------------------+---------------------------------+-----------------+\n");
-	double x = 0.0;
-	for (int i = 0; i <= n; i++) {
-		x = d * i;
-		double math_func = exp(pow(x, 2));
-		double res = 0;
-		int n1 = 0;
-		double numerator = 1;
-		double denominator = 1;
-		for(;;n1++)
-		{
-			double cur = numerator / denominator;
-			if(fabs(cur) < e0)
-				break;
-			res += cur;
-			numerator *= x * x;
-			denominator *= fact(n1 + 1);
-		}
-		printf("| %.3f\t|          %.19f          |      %.19f      |        %d        |\n", x, res, math_func, n1);
-	}
-	printf("+-------+-----------------------------------------+---------------------------------+-----------------+\n");
-	return 0;
+    printf("Введите количество равных разбиений отрезка [a, b]: n = ");
+    scanf("%d", &n);
+    if (n < 1) {
+        printf("Введенное значение должно быть больше нуля!\n");
+        return 0;
+    }
+
+    long double e0 = machine_epsilon(x);
+    printf("Машинное эпсилон для x = %.10Le: e = %.10Le\n", x, e0);
+
+    printf("+-------------------------------------------------------------------------------------------------------+\n");
+    printf("|            Таблица значений ряда Тейлора и стандартной функции для f(x) = exp(pow(x, 2))              |\n");
+    printf("+-------+-----------------------------------------+-----------------------------------+-----------------+\n");
+    printf("|   x   | частичная сумма ряда для exp(pow(x, 2)) |      значение функции f(x)        | число итераций  |\n");
+    printf("+-------+-----------------------------------------+-----------------------------------+-----------------+\n");
+
+
+    long double d = 1.0L / (long double)n;
+
+    long double x_value = 0.0L;
+    for (int i = 0; i <= n; i++) {
+        x_value = d * i;
+        long double math_func = expl(x_value * x_value);
+
+        long double res = 1.0L;
+        long double term = 1.0L;
+        int n1 = 0;
+
+        while (fabsl(term) > e0) {
+            term *= x_value * x_value / (n1 + 1);
+            res += term;
+
+            n1++;
+        }
+
+        printf("| %.3Lf\t|     %.19Le           |     %.19Le     |     %d          |\n", x_value, res, math_func, n1);
+    }
+
+    printf("+-------+-----------------------------------------+-----------------------------------+-----------------+\n");
+    return 0;
 }
